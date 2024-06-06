3bbbc17e06c94c0bdcbedcd158d6a226f5e2a945
Luan Fernandes <souluanf@icloud.com>
2024-06-06 13:32:19 -0300

    feat(service): implementar edição de produtos existentes

--- src/dev/luanfernandes/store/service/ProdutoService.java
+++ src/dev/luanfernandes/store/service/ProdutoService.java
@@ -2,4 +2,6 @@ package dev.luanfernandes.store.service;
 
 public interface ProdutoService {
     void cadastrarProduto(String nome, double valor, int quantidade);
+
+    void editarProduto(int id, String nome, double valor, int quantidade);
 }
--- src/dev/luanfernandes/store/service/impl/ProdutoServiceImpl.java
+++ src/dev/luanfernandes/store/service/impl/ProdutoServiceImpl.java
@@ -5,6 +5,8 @@ import dev.luanfernandes.store.repository.ProdutoRepository;
 import dev.luanfernandes.store.repository.impl.ProdutoRepositoryImpl;
 import dev.luanfernandes.store.service.ProdutoService;
 
+import java.util.Optional;
+
 public class ProdutoServiceImpl implements ProdutoService {
     private static ProdutoServiceImpl instance;
     private final ProdutoRepository produtoRepository = ProdutoRepositoryImpl.getInstance();
@@ -24,5 +26,17 @@ public class ProdutoServiceImpl implements ProdutoService {
         Produto produto = new Produto(produtoRepository.obterProximoId(), nome, valor, quantidade);
         produtoRepository.salvar(produto);
         System.out.println("PRODUTO SALVO!");
+    }
+
+    @Override
+    public void editarProduto(int id, String nome, double valor, int quantidade) {
+        Optional<Produto> produtoOptional = produtoRepository.buscarPorId(id);
+        if (produtoOptional.isPresent()) {
+            Produto produto = new Produto(id, nome, valor, quantidade);
+            produtoRepository.salvar(produto);
+            System.out.println("PRODUTO EDITADO COM SUCESSO!");
+        } else {
+            System.out.println("PRODUTO NÃO ENCONTRADO!");
+        }
     }
 }
